c2f26ca5b1ddfa90b1ff82ab9dcb0bbd12bfb947
Szeng89 <shijinz898@gmail.com>
2024-04-02 23:25:44 -0400

    added LinkedListTest

--- gettestie/src/test/java/rocks/zipcode/LinkedListTest.java
+++ gettestie/src/test/java/rocks/zipcode/LinkedListTest.java
@@ -1,14 +1,29 @@
 package rocks.zipcode;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.LinkedList;
 
 public class LinkedListTest {
+
     @Test
     public void testAddAndRetrieve() {
         LinkedList<String> list = new LinkedList<>(); // Create new LinkedList for Strings
+        list.add("Zip"); // last in last out
+        list.add("Code"); // so Code should be last
+
+        Assert.assertEquals("Code", list.get(1));
+    }
+
+    @Test
+    public void retrieveTest() {
+        LinkedList<String> list = new LinkedList<>();
         list.add("Zip");
         list.add("Code");
+        list.add(1, "Shijin");
+
+        Assert.assertEquals("Shijin", list.get(1));
     }
 }
